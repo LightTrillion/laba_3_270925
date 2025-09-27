1c5cfe0fc4e332c99a3dd01c76b1e859dc5f7100
Vorobevev <Vorobevev@202D-11>
2025-09-27 14:38:54 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace laba_3_270925
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            if (textBox.Text == "Пример текста")
+            {
+                textBox.Text = ""; // Очищаем текст, чтобы он исчез
+            }
+            else
+            {
+                textBox.Text = "Пример текста";
+            }
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var zd_4 = new Window1();
+            zd_4.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+namespace laba_3_270925
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_close(object sender, RoutedEventArgs e)
+        {
+            var zd_2_3 = new MainWindow();
+            zd_2_3.Show();
+            this.Close();
+        }
+        private void ClearCanvas_Click(object sender, RoutedEventArgs e)
+        {
+            drawingCanvas.Strokes.Clear();
+        }
+    }
+
+    public class EditingModeToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is InkCanvasEditingMode mode)
+            {
+                switch (mode)
+                {
+                    case InkCanvasEditingMode.Ink: return "Рисование";
+                    case InkCanvasEditingMode.EraseByPoint: return "Редактирование";
+                    default: return mode.ToString();
+                }
+            }
+            return value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
